9b75edc51608185a67ccb6f85e440b6078585a6e
Aaron David <ajdavid128@gmail.com>
2023-05-02 10:18:27 -0500

    made successful fetch from backend to park route and console logged information

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+
+useEffect(() => {
+  // FIRGURE OUT HOW TO OVERRIDE LIMIT ON FETCH TO HAVE ACCESS TO ALL PARKS
+  fetch('http://localhost:3000/parks')
+  .then(res => res.json())
+  .then(console.log) 
+},[])
+
   return (
     <div className="App">
       <h1>Hi, I'm Davie Crockett and welcome to the Alamo!</h1>
+
     </div>
   );
 }
