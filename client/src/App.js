7c55487c8aa5287fd5ef6be76598285091d23c91
Aaron David <ajdavid128@gmail.com>
2023-05-15 15:53:11 -0500

    testing how to grab the object from national parks API and convert it to an array

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 
 function App() {
 
+  const [data, setData] = useState([])
+
 useEffect(() => {
   // FIRGURE OUT HOW TO OVERRIDE LIMIT ON FETCH TO HAVE ACCESS TO ALL PARKS
   fetch('http://localhost:3000/parks')
   .then(res => res.json())
-  .then(console.log) 
+  .then(data => setData(data.data))
 },[])
+
+console.log(data)
 
   return (
     <div className="App">
