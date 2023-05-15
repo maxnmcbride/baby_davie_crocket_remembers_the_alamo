9be7f41672ea69c987285c119a0f6e31315caf9c
Keila Lopez <keilalofra@gmail.com>
2023-05-15 15:18:04 -0600

    Created form structure according to wireframe. Styling pending

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,13 @@
 class SessionsController < ApplicationController
+    
+    # User login, custom route
     def create
     # looks up a user in the database using params (from frontend form)
-    user = User.find_by(username: params[:username])
-
     # verify the login credentials
-    session[:user_id] = user.id
-
     # store the authenticated user’s id in the session
+    user = User.find_by(username: params[:username])
+    session[:user_id] = user.id
     render json: user
-
     end
 
     # logout custom route
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,7 +2,34 @@
 function SignUp () {
     return (
         <div>
-            Testing Sign Up
+            <form>
+                <input 
+                    type='text'
+                    name='name'
+                    placeholder='Name'
+                />
+                <input 
+                    type='text'
+                    name='lastName'
+                    placeholder='Last Name'
+                />
+                <input 
+                    type='text'
+                    name='username'
+                    placeholder='Username'
+                />
+                <input 
+                    type='text'
+                    name='password'
+                    placeholder='Create Password'
+                />
+                <input 
+                    type='text'
+                    name='confirmPassword'
+                    placeholder='Confirm Password'
+                />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
