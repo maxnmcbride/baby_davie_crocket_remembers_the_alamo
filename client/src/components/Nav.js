84d5a13e87b248d53c4c5837f091f186d1217f85
Aaron David <ajdavid128@gmail.com>
2023-05-16 14:39:30 -0500

    added links to all pages in Nav component for easy traversal

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,6 +10,18 @@ function Nav() {
             <Link to="/signup">Sign Up</Link>
             <br/>
             <Link to="/about">About</Link>
+            <br/>
+            <Link to="/state">State</Link>
+            <br/>
+            <Link to="/park">Park</Link>
+            <br/>
+            <Link to="/surprise">Surprise</Link>
+            <br/>
+            <Link to="/login">Login</Link>
+            <br/>
+            <Link to="/dashboard">Dashboard</Link>
+            <br/>
+            <Link to="/top-rated">Top Rated</Link>
             <Outlet />
         </div> 
     )
