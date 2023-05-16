2ad9028e2547caa81be5080b04d4e06615d8c74a
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-05-16 14:50:52 -0400

    working on adding npm packages for mapbox and react-map-gl. added in prefab component from documentation example

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
+// import Map from 'react-map-gl';
 
 function App() {
 
   const [parkData, setParkData] = useState([])
-  
+
   useEffect(() => {
     // FIGURE OUT HOW TO OVERRIDE LIMIT ON FETCH TO HAVE ACCESS TO ALL PARKS
     fetch('http://localhost:3000/parks')
@@ -15,11 +16,21 @@ function App() {
       .then(data => setParkData(data.data))
   }, [])
 
-  console.log(parkData.map((parkObj) => parkObj.fullName))
+  console.log(parkData.map((parkObj) => parkObj))
 
   return (
     <div className="App">
       <h1>Hi, I'm Davie Crockett and welcome to the Alamo!</h1>
+      {/* <Map
+        mapLib={import('mapbox-gl')}
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />; */}
     </div>
   );
 
