e96ef1a817d955307fb99688dee4ea6e8c966b5b
Aaron David <ajdavid128@gmail.com>
2023-05-23 11:36:18 -0500

    built skeleton for about page with sections, img designations, headers and placeholders

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,69 @@ function About() {
         <>
             <h1>ABOUT THE PROJECT PAGE DUMMY</h1>
             <img id='about-page-banner' src={placeholder} alt='park prowler banner'/>
+            <section> 
+                <h1>What is Park Prowler?</h1>
+                <p>Whos-a-whats-it is a cool person and they do... Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
+            </section>
+            <section>
+                <h1>Tech Stack</h1>
+                <section>
+                    <div>
+                        <img src='' alt=''/>
+                        <h6>tech</h6>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h6>tech</h6>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h6>tech</h6>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h6>tech</h6>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h6>tech</h6>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h6>tech</h6>
+                    </div>
+                </section>
+            </section>
+            <section>
+                <h1>Meet The Prowler Team!</h1>
+                <section>
+                    <div>
+                        <img src='' alt=''/>
+                        <h4>name</h4>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h4>name</h4>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h4>name</h4>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h4>name</h4>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <h4>name</h4>
+                    </div>
 
+                </section>
+            </section>
+            <hr></hr>
+            <section>
+                {}
+            </section>
         </>
      );
 }
