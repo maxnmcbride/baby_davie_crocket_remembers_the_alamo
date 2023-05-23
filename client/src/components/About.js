2aee01ee32cd906f835ea8619da40a7f9e472e22
Aaron David <ajdavid128@gmail.com>
2023-05-23 12:10:17 -0500

    added class names to tech stack icons and styled for max-height

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -13,37 +13,37 @@ function About() {
                 <h1>Tech Stack</h1>
                 <section>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='react icon'/>
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='react icon'/>
                     </div>
                     <div> 
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='javascript icon'/>
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='javascript icon'/>
                     </div>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain-wordmark.svg" alt='ruby icon'/>
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain-wordmark.svg" alt='ruby icon'/>
                     </div>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain-wordmark.svg" alt='rails icon' />
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain-wordmark.svg" alt='rails icon' />
                     </div>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg" alt='sqlite icon'/>
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg" alt='sqlite icon'/>
                     </div>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt='html5 icon'/>
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt='html5 icon'/>
                     </div>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt='css3 icon'/>
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt='css3 icon'/>
                     </div>
                     <div>
-                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt='figma icon'/>
+                         <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt='figma icon'/>
                     </div>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='github logo' />
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='github logo' />
                     </div>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt='npm icon' />
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt='npm icon' />
                     </div>
                     <div>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg" alt='visual studio code icon'/>
+                        <img className='tech-stack-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg" alt='visual studio code icon'/>
                     </div>
                 </section>
             </section>
