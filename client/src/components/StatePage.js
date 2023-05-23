2489b14e58d7e6a958f5aba1dbe9597eda890cce
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-05-23 12:31:28 -0400

    started adding bare structure to state page

--- client/src/components/StatePage.js
+++ client/src/components/StatePage.js
@@ -1,6 +1,19 @@
 function StatePage() {
     return ( 
-        <h1>State Page</h1>
+        <div id="state-container">
+            <div id="state-header">
+                <h1>State Name</h1>
+            </div>
+            <div id="state-content">
+                <h3>Lorem epsum blah blah  here’s some intro about this state. Saying other stuff about this state it’s super cool. Okeydokey. </h3>
+                <div id="park-thumbnails"></div>
+                <div className="parks-display"></div>
+                <div className="parks-display"></div>
+
+            
+            </div>
+
+        </div>
         //test
      );
 }
