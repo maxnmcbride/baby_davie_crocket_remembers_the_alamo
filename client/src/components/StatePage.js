9b417557387863a955e7ee5432236cec4729f4ca
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-05-23 12:45:29 -0400

    added test fetch for thumbnails

--- client/src/components/StatePage.js
+++ client/src/components/StatePage.js
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 function StatePage() {
+    const [data, setData] = useState([])
+
+    //TEST - REPLACE WITH PARK FETCH
+    useEffect(() => {
+        fetch('http://shibe.online/api/shibes?count=5')
+          .then(res => res.json())
+          .then(data => setData(data))
+      }, [])
+
+
+    
+    function ParkThumbnails ({data}) {
+        return(
+            <img src={data} alt="test" style={{width: 100}} />
+        )
+    }
+
     return ( 
         <div id="state-container">
             <div id="state-header">
@@ -6,15 +25,15 @@ function StatePage() {
             </div>
             <div id="state-content">
                 <h3>Lorem epsum blah blah  here’s some intro about this state. Saying other stuff about this state it’s super cool. Okeydokey. </h3>
-                <div id="park-thumbnails"></div>
+                <div id="park-thumbnails">
+                   {data ? data.map((d) => <ParkThumbnails data={d}/>) : null}
+                </div>
                 <div className="parks-display"></div>
                 <div className="parks-display"></div>
-
-            
             </div>
 
         </div>
-        //test
+        
      );
 }
 
