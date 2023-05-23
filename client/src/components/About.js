cc26f5a10d30f73cdf8f03ea0c95ee0104dfe55d
Aaron David <ajdavid128@gmail.com>
2023-05-23 11:20:06 -0500

    added media folder to src directory, added dividers to CSS file for specific pages, began buidling out about page skeleton

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,6 +1,12 @@
+import placeholder from '../media/placeholder.png';
+
 function About() {
     return ( 
-        <h1>ABOUT THE PROJECT PAGE DUMMY</h1>
+        <>
+            <h1>ABOUT THE PROJECT PAGE DUMMY</h1>
+            <img id='about-page-banner' src={placeholder} alt='park prowler banner'/>
+
+        </>
      );
 }
 
